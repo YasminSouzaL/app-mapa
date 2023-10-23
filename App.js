3f9300c539b7a28a4ad36e8ef4a7d631273ff8ac
Yasmin <souzasmimi@gmail.com>
2023-10-23 10:49:58 -0300

    teste_mapView

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {MapView,MapMarker} from 'react-native-maps';
 
-export default function App() {
+function App(){
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Mapa</Text>
+      <View style={{height: 100, width: 100, backgroundColor: 'red'}}>
+        <MapView style={{flex: 1}}>
+          <MapMarker coordinate={{latitude: 37.78825, longitude: -122.4324}}/>
+        </MapView>
+      </View>
     </View>
   );
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
