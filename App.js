214a1db7ab72ab315808fe15c6f30fe4f0cc4360
Yasmin <souzasmimi@gmail.com>
2023-10-28 14:58:31 -0300

    App1

--- App.js
+++ App.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import {MapView} from 'react-native-maps';
+import { MapView, Marker } from 'react-native-maps';
 
-export default function App(){
-  return(
+export default function App() {
+  return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <MapView style={{flex:1}}/>
+      <MapView style={{ width: 400, height: 400 }}
+        region={{
+          latitude: -19.19700,
+          longitude: -46.23719,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}
+      >
+        <Marker
+          coordinate={{
+            latitude: -19.19700,
+            longitude: -46.23719,
+          }}
+          title="Casa"
+          description="Minha casa"
+        />
+      </MapView>
     </View>
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
